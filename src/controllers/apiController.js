f0b898baa418138bb8d0c72205eb6d5a26b309b5
ericddd09 <eric@hambox.com.au>
2019-05-25 11:22:10 +1000

    API documentation

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -41,4 +41,76 @@ router.get('/mail/:id', validate(validator.getMail), (req, res, next) => {
   }).catch(next)
 })
 
+router.get('/', (req, res) => {
+  res.status(status.OK)
+  res.setHeader('Content-Type', 'text/plain')
+  res.send(`
+  
+    Simple Mail Service
+    
+    @author - eric@hambox.com.au
+    @feedback - https://github.com/ericdybal/simple-mail-service
+  
+    Usage:
+      [Send Email] 
+    
+      POST /mail HTTP/1.1 
+      Host: https://hambox.com.au/simple-mail-service
+      Content-Type: application/x-www-form-urlencoded 
+
+      Supported Parameters:
+      
+        * from    - email sender (mandatory)
+        * to      - comma separated list of recipients (mandatory)
+        * cc      - comma separated list of recipients
+        * bcc     - comma separated list of recipients
+        * subject - subject header
+        * text    - email content, text only
+          
+      Response:
+      
+       200 - Success
+        
+        * header[location] - URL to check the email delivery status 
+        * body    - See [Get Email Status] response      
+              
+       400 - Bad Request
+       
+        * code    - 400 
+        * message - error message 
+        * errors  - list of errors where each error contains the following fields:
+           * location - [body | param] 
+           * message  - field specific error message  
+           * param    - field name 
+           * value    - field value
+
+
+      [Get Email Status]
+
+      GET /mail HTTP/1.1 
+      Host: https://hambox.com.au/simple-mail-service
+      Content-Type: application/json 
+
+      Supported Parameters:
+      
+       * id       - unique email id (mandatory)
+       
+       Response:
+       
+       200 - Success
+       
+       * id           - unique email id
+       * dateCreated  - date email submitted for delivery
+       * dateSend     - date email actually delivered
+       * status       - delivery status [PENDING - waiting to be delivered | COMPLETED - delivered | FAILED - failed to deliver] 
+       * errorCount   - failed delivery count (will retry upto 3 times)
+       * message      - email message (see above)
+       
+       400 - Bad Request 
+       
+       * See [Send Email]
+       
+    `)
+})
+
 export default router
